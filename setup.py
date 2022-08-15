706faf1356c32c428fc318ac8510407703e21159
minggnim <38852608+minggnim@users.noreply.github.com>
2022-08-15 21:29:15 +0100

    update project setup

--- setup.py
+++ setup.py
@@ -1,15 +1,2 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='bert-classifier',
-    packages=find_packages(where='src.bert_classifier'),
-    package_dir={'': 'src'},
-    version='0.1.0',
-    description='Bert based NLP classification models',
-    author='ming_gao@outlook.com',
-    license='MIT',
-    install_requires=[
-        'torch',
-        'transformers',
-    ]
-)
+import setuptools
+setuptools.setup()
