12837b7f71c89baf1520fd73b0a14c4e5dd6a7f5
minggnim <38852608+minggnim@users.noreply.github.com>
2023-08-07 11:37:25 +0100

    update README and gitignore

--- src/nlp_models/llm/vectordbs.py
+++ src/nlp_models/llm/vectordbs.py
@@ -5,11 +5,14 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from .base import LlmConfig
 
 
-def build_vectordb(config: LlmConfig):
-    loader = DirectoryLoader(config.DATA_PATH, glob='*.pdf', loader_cls=PyPDFLoader)
-    docs = loader.load()
+def build_vectordb(config: LlmConfig, file=None):
+    if not file:
+        loader = DirectoryLoader(config.DATA_PATH, glob='*.pdf', loader_cls=PyPDFLoader)
+        docs = loader.load()
+    else:
+        docs = file
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=config.CHUNK_SIZE, chunk_overlap=config.CHUNK_OVERLAP)
-    texts = text_splitter.split_documents(docs)
+    texts = text_splitter.create_documents(docs)
     embeddings = HuggingFaceEmbeddings(model_name=config.EMBEDDING_MODEL, model_kwargs={'device': 'cpu'})
     vector_store = FAISS.from_documents(texts, embeddings)
     vector_store.save_local(config.FAISS_DB_PATH)
